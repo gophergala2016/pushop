dcb24b169e4139a182b76fd2b2d97481b9d314e9
aquilax <aquilax@gmail.com>
2016-01-23 12:30:44 +0100

    Updated main

--- main.go
+++ main.go
@@ -1,4 +1,45 @@
 package main
 
+import (
+	"os"
+
+	"github.com/codegangsta/cli"
+)
+
+const (
+	appName    = "Pushop"
+	appUsage   = "Command line image gallery generator"
+	appVersion = "0.0.1"
+)
+
 func main() {
+	app := cli.NewApp()
+	app.Name = appName
+	app.Usage = appUsage
+	app.Version = appVersion
+
+	app.Commands = []cli.Command{
+		{
+			Name:  "init",
+			Usage: "Initializes new project",
+			Action: func(c *cli.Context) {
+				println("init")
+			},
+		},
+		{
+			Name:  "build",
+			Usage: "Builds the gallery",
+			Action: func(c *cli.Context) {
+				println("build")
+			},
+		},
+		{
+			Name:  "serve",
+			Usage: "Builds and serves the gallery",
+			Action: func(c *cli.Context) {
+				println("serve")
+			},
+		},
+	}
+	app.Run(os.Args)
 }
