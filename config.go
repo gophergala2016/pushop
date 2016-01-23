110e94a9fa662100b85979286e96e17617f805de
aquilax <aquilax@gmail.com>
2016-01-23 13:05:26 +0100

    Basic file structure

--- config.go
+++ config.go
@@ -1,8 +1,33 @@
 package main
 
+import (
+	"io"
+)
+
 type Config struct {
+	Title       string  `yaml:"title"`
+	Description string  `yaml:"description"`
+	Author      *string `yaml:"author"`
+	Analytics   *string `yaml:"analytics"`
+	Content     *[]File `yaml:"content"`
+}
+
+type File struct {
+	Name        *string   `yaml:"name"`
+	Description *string   `yaml:"description"`
+	Tags        *[]string `yaml:"tags"`
+	Exif        *bool     `yaml:"exif"`
+	Permalink   *string   `yaml:"permaling"`
 }
 
 func newConfig() *Config {
 	return &Config{}
+}
+
+func (c *Config) load(reader io.Reader) error {
+	return nil
+}
+
+func (c *Config) save(writer io.WriteCloser) error {
+	return nil
 }
