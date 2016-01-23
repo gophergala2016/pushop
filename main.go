d2df24d0415b98c56af8cdfdea2cd319c3dd947a
aquilax <aquilax@gmail.com>
2016-01-23 14:14:56 +0100

    Generate configuration file closes #2

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"bufio"
 	"os"
 
 	"github.com/codegangsta/cli"
@@ -18,12 +19,44 @@ func main() {
 	app.Usage = appUsage
 	app.Version = appVersion
 
+	config := newConfig()
+
 	app.Commands = []cli.Command{
 		{
 			Name:  "init",
 			Usage: "Initializes new project",
 			Action: func(c *cli.Context) {
-				println("init")
+				var err error
+				dirname := c.Args().First()
+				if dirname == "" {
+					if dirname, err = os.Getwd(); err != nil {
+						panic(err)
+					}
+				}
+				// TODO: Check if directory is valid
+				if err = config.generate(dirname); err != nil {
+					panic(err)
+				}
+				// TODO: Make the path OS agnostic
+				file, err := os.Create(dirname + "/" + defaultConfigFileName)
+				defer file.Close()
+				if err != nil {
+					panic(err)
+				}
+				w := bufio.NewWriter(file)
+				err = config.save(w)
+				if err != nil {
+					panic(err)
+				}
+				w.Flush()
+				file.Close()
+			},
+		},
+		{
+			Name:  "updates",
+			Usage: "Updates existing project",
+			Action: func(c *cli.Context) {
+				println("update")
 			},
 		},
 		{
